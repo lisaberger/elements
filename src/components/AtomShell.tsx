93a7b918974af1e00655e21a1c5ef89c8b58f91e
bergerli <lisa.berger@hs-augsburg.de>
2024-03-04 19:40:12 +0100

    refactor: update atom shell structure

--- src/components/AtomShell.tsx
+++ src/components/AtomShell.tsx
@@ -1,68 +1,58 @@
-import { FC, useRef } from 'react';
-import * as THREE from 'three';
-
-/**
- * Adds electron shells to the 3D representation of an atom.
- * Shells are created based on the atom's electron distribution.
- */
+import { FC } from 'react';
+import { DoubleSide } from 'three';
 
 interface AtomShellProps {
     electronDistribution: Array<number>;
 }
 
 const AtomShell: FC<AtomShellProps> = ({ electronDistribution }) => {
-    const shellRef = useRef<THREE.Group>();
-
-    const pivot = new THREE.Object3D();
-    if (shellRef.current) {
-        shellRef.current.add(pivot);
-    }
-
-    let xPos = 0;
-    let yPos = 0;
-    let radius = 1500;
+    const radius = 1500;
     const distance = 1000;
 
-    for (let s = 0; s < electronDistribution.length; s++) {
-        for (let n = 0; n < electronDistribution[s]; n++) {
-            xPos =
-                radius * Math.cos((n / electronDistribution[s]) * 2 * Math.PI);
-            yPos =
-                radius * Math.sin((n / electronDistribution[s]) * 2 * Math.PI);
+    return (
+        <group>
+            {electronDistribution.map((shellElectronCount, shellIndex) => {
+                // Create shells
+                const shellRadius = radius + shellIndex * distance;
 
-            const electronGeometry = new THREE.SphereGeometry(80, 50, 16);
-            const electronMaterial = new THREE.MeshBasicMaterial({
-                color: 0xffffff,
-            });
+                const shells = (
+                    <mesh key={`shell_${shellIndex}`}>
+                        <ringGeometry
+                            args={[shellRadius, shellRadius + 10, 80]}
+                        />
+                        <meshBasicMaterial color={0x816cff} side={DoubleSide} />
+                    </mesh>
+                );
 
-            const shellGeometry = new THREE.RingGeometry(
-                radius,
-                radius + 10,
-                80,
-            );
-            const shellMaterial = new THREE.MeshBasicMaterial({
-                color: 0x816cff,
-                side: THREE.DoubleSide,
-            });
-            const shell = new THREE.Mesh(shellGeometry, shellMaterial);
-            if (shellRef.current) {
-                shellRef.current.add(shell);
-            }
+                // Create and place electrons on shells
+                const electrons = Array.from({
+                    length: shellElectronCount,
+                }).map((_, electronIndex) => {
+                    const angle =
+                        (electronIndex / shellElectronCount) * 2 * Math.PI;
+                    const xPos = shellRadius * Math.cos(angle);
+                    const yPos = shellRadius * Math.sin(angle);
 
-            const electron = new THREE.Mesh(electronGeometry, electronMaterial);
+                    return (
+                        <mesh
+                            key={`electron_${shellIndex}_${electronIndex}`}
+                            position={[xPos, yPos, 0]}
+                        >
+                            <sphereGeometry args={[80, 50, 16]} />
+                            <meshBasicMaterial color={0xffffff} />
+                        </mesh>
+                    );
+                });
 
-            pivot.add(electron);
-
-            electron.position.set(xPos, yPos, 0);
-
-            if (shellRef.current) {
-                shellRef.current.add(electron);
-            }
-        }
-        radius += distance;
-    }
-
-    return <group ref={shellRef} />;
+                return (
+                    <group key={`shell_group_${shellIndex}`}>
+                        {shells}
+                        {electrons}
+                    </group>
+                );
+            })}
+        </group>
+    );
 };
 
 export default AtomShell;
