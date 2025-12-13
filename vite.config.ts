b52b25fa303f17df57915232db14b6c75bb040ac
lisaberger <lisa.berger97@web.de>
2025-12-13 20:21:44 +0100

    chore: adjust it again

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig(({ mode }) => ({
     plugins: [react(), tailwindcss()],
+    base: process.env.NODE_ENV === 'production' ? '/elements' : '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
