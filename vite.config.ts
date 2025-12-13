136168daa7cc5723afca3c92c3133c07ce481581
lisaberger <lisa.berger97@web.de>
2025-12-13 19:42:46 +0100

    chore: remove base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: '/elements',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
